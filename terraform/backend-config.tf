aeba13089d3575f51ab9d7e375c9d1d61215d53a
Lucas Livrone <lucaslivrone@gmail.com>
2023-10-11 23:58:42 -0300

    Updated terraform init -backend-config

--- terraform/backend-config.tf
+++
@@ -1,5 +0,0 @@
-bucket         = var.tf_state_backend_bucket
-key            = var.tf_state_backend_key
-region         = var.aws_region
-encrypt        = true
-dynamodb_table = var.tf_state_lock
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,4 @@
 # Use S3 backend for storing the State file and State lock
-# Since variables cannot be used while defining this block, required details will be located at backend-config.tf
-# Required startup command: terraform init -backend-config=backend-config.tf
 terraform {
   backend "s3" {}
 }
